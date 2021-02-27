e309c0aed977a07f469ff500044766c19177457a
Arris Li <arrisli@iqhive.com>
2021-02-27 16:48:40 +1300

    two sum basic

---
+++ two_sums/two_sum_basic.go
@@ -0,0 +1,15 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	for i := range nums {
+		for j := range nums {
+			if j == i {
+				continue
+			}
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
