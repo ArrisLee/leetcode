c01bc2810287ba4f96ebcc34f3e0faf7d2bcb9bd
Arris Li <arrisli@iqhive.com>
2021-02-28 23:36:48 +1300

    update solution

--- reverse_integer/reverse_integer.go
+++ reverse_integer/reverse_integer.go
@@ -8,12 +8,13 @@ import (
 func reverse(x int) int {
 	orig := strconv.Itoa(x)
 	numStr := ""
-	for i := len(orig) - 1; i >= 0; i-- {
-		if i == 0 && string(orig[i]) == "-" {
-			numStr = "-" + numStr
-		} else {
-			numStr += string(orig[i])
-		}
+	end := 0
+	if string(orig[0]) == "-" {
+		numStr = "-"
+		end = 1
+	}
+	for i := len(orig) - 1; i >= end; i-- {
+		numStr += string(orig[i])
 	}
 	if result, _ := strconv.Atoi(numStr); result < math.MinInt32 || result > math.MaxInt32 {
 		return 0
