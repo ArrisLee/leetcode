abe9c23fe70b82baef58ed17ae0b320d73822638
Arris Li <arrisli@iqhive.com>
2021-03-01 22:22:32 +1300

    updated two_sum solution

---
+++ two_sums/two_sum_test.go
@@ -0,0 +1,49 @@
+package twosums
+
+import "testing"
+
+func TestTwoSumSltnOne(t *testing.T) {
+	want := map[int]bool{
+		0: true,
+		1: true,
+	}
+	got := twoSumSltnOne([]int{2, 7, 11, 15}, 9)
+	for i := range got {
+		if !want[got[i]] {
+			t.Errorf("want: %v, got: %v", want, got)
+		}
+	}
+	got = twoSumSltnOne([]int{3, 2, 4}, 6)
+	want = map[int]bool{
+		1: true,
+		2: true,
+	}
+	for i := range got {
+		if !want[got[i]] {
+			t.Errorf("want: %v, got: %v", want, got)
+		}
+	}
+}
+
+func TestTwoSumSltnTwo(t *testing.T) {
+	want := map[int]bool{
+		2: true,
+		3: true,
+	}
+	got := twoSumSltnTwo([]int{2, 7, 11, 15}, 26)
+	for i := range got {
+		if !want[got[i]] {
+			t.Errorf("want: %v, got: %v", want, got)
+		}
+	}
+	got = twoSumSltnTwo([]int{3, 2, 4}, 6)
+	want = map[int]bool{
+		1: true,
+		2: true,
+	}
+	for i := range got {
+		if !want[got[i]] {
+			t.Errorf("want: %v, got: %v", want, got)
+		}
+	}
+}
