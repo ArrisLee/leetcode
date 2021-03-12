b3da08f93855a0686c33843ed06cf5d0370dc227
ArrisLee <arris_li@hotmail.com>
2021-03-12 21:52:13 +1300

    test workflow

--- two_sums/two_sum.go
+++ two_sums/two_sum.go
@@ -1,20 +1,6 @@
 package twosums
 
-func twoSumSltnOne(nums []int, target int) []int {
-	for i := range nums {
-		for j := range nums {
-			if j == i {
-				continue
-			}
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
-		}
-	}
-	return nil
-}
-
-func twoSumSltnTwo(nums []int, target int) []int {
+func twoSum(nums []int, target int) []int {
 	numMap := make(map[int]int)
 	for i := range nums {
 		if val, ok := numMap[target-nums[i]]; ok {
